babdaa0e1418456169790da16d087362ec807b68
shuhala <sophiebmercier@hotmail.com>
2017-10-15 15:35:18 -0400

    i3: add scrot shortcuts for screenshots

--- .config/shuhala/alias.zsh
+++ .config/shuhala/alias.zsh
@@ -2,15 +2,14 @@
 alias sudo='sudo '
 alias ll="ls -lhA"
 alias c="clear"
-alias sc='xset r rate 250 50'
-alias sc1='xset r rate 250 75'
-alias sc2='xset r rate 200 100'
+alias sc='xset r rate 200 100'
 alias h='history'
 alias j='jobs -l'
-alias claim_dir='sudo chown -R shuhala:shuhala ./'
+alias claim_dir='sudo chown -R $USER ./'
 
 # Configs
-alias aedit='vim ~/.dotfiles/alias.zsh'
+alias aedit='vim ~/.config/$USER/alias.zsh'
+alias i3edit='vim ~/.config/i3/config'
 alias vedit='vim ~/.vimrc'
 alias tedit='vim ~/.tmux.conf'
 alias zedit='vim ~/.zshrc'
@@ -23,27 +22,21 @@ alias tls='tmux ls'
 alias tkill='tmux kill-session -t'
 
 # Docker
-alias docker-clean='sudo docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
-alias docker-purge='docker-purge_images && docker-purge_volumes'
-alias docker-purge_images='sudo docker rmi $(docker images -f dangling=true -q)'
-alias docker-purge_volumes='sudo docker volume rm $(docker volume ls -f dangling=true -q)'
-
-# Apt
-alias apt-get="sudo apt-get"
-alias update='sudo apt-get update -y && sudo apt-get upgrade -y'
+alias docker='sudo docker'
+alias docker-clean='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
 
 # System
 alias reboot='sudo /sbin/reboot'
 alias shutdown='sudo /sbin/shutdown'
 
 # Cheatsheets
-alias cs-apt='cat ~/.cheatsheet/apt'
-alias cs-neobundle='cat ~/.cheatsheet/neobundle'
-alias cs-git='cat ~/.cheatsheet/git'
-alias cs-git_varia='cat ~/.cheatsheet/git-varia'
-alias cs-tmux='less ~/.cheatsheet/tmux'
+alias cs-apt='cat ~/.cheatsheets/apt'
+alias cs-neobundle='cat ~/.cheatsheets/neobundle'
+alias cs-git='cat ~/.cheatsheets/git'
+alias cs-git_varia='cat ~/.cheatsheets/git-varia'
+alias cs-tmux='less ~/.cheatsheets/tmux'
 
-# Server
-alias connect_shuhala-server='ssh shuhala@192.168.0.191'
-alias mount_shuhala-server='sudo mount -t cifs -o username=shuhala //192.168.0.191/Data /mnt/Data/ && sudo mount -t cifs -o username=shuhala //192.168.0.191/Media /mnt/Media/'
+# Varia
+alias KILLDERBY='rm -f ~/.m2/repository/org/apache/derby/derby/10.11.1.1/derby-10.11.1.1.ja*'
+alias nspire='wine start "C:\Program Files (x86)\TI Education\TI-Nspire CX CAS Student Software\TI-Nspire CX CAS Student Software.exe"'
 

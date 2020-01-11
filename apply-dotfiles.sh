17b08e3ebe5f88beff14fd41a73802f11c5697aa
Sophie B-Mercier <sophiebmercier@hotmail.com>
2020-01-10 23:05:13 -0500

    add apply-dotfiles script

---
+++ apply-dotfiles.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function applyConfig {
+  # args: $from, $to
+  read -p "Copy $1? (y/N)" -n 1 yn
+  echo ""
+  case $yn in
+    [Yy]* ) cp -r $1 $2 ;;
+    * ) ;;
+  esac
+}
+
+function applyConfigRoot {
+  # args: $from, $to
+  read -p "Copy $1? (y/N)" -n 1 yn
+  echo ""
+  case $yn in
+    [Yy]* ) sudo cp -r $1 $2 ;;
+    * ) ;;
+  esac
+}
+
+applyConfig .Xresources $HOME
+applyConfig .bashrc $HOME
+applyConfig .config/$USER $HOME/.config/$USER
+applyConfig .config/betteri3lock.sh $HOME/.config/betteri3lock
+applyConfig .config/compton.conf $HOME/.config/compton
+applyConfig .config/i3 $HOME/.config/i3
+applyConfig .config/ranger $HOME/.config/ranger
+applyConfig .i3blocks.conf $HOME
+applyConfig .tmux.conf $HOME
+applyConfig .vimrc $HOME
+applyConfig .xinitrc $HOME
+applyConfig .xres $HOME
+applyConfig .zshrc $HOME
+applyConfigRoot ./var/lib/connman /var/lib/connman
